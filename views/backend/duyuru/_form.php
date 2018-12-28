1fd26bab8c047ba5b067ccb8106752213aa1c384
Furkan Faruk Ekinci <ffekinci@gmail.com>
2018-12-28 23:53:35 +0300

    dropdown update

--- views/backend/duyuru/_form.php
+++ views/backend/duyuru/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kouosl\duyuru\models\Kategori;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model vendor\kouosl\duyuru\models\Duyuru */
@@ -18,7 +20,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
-    <?= $form->field($model, 'kat_id')->textInput() ?>
+    <?= $form->field($model, 'kat_id')->dropDownList(ArrayHelper::map(Kategori::find()->all(), 'id', 'title'), ['prompt'=>'Kategori Seçiniz']) ?>
 
     <?= $form->field($model, 'status')->dropDownList([ 'active' => 'Active', 'passive' => 'Passive', ], ['prompt' => '']) ?>
 
